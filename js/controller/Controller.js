36a13ed8c64597c06dfa8e16df76df3f63bbe803
Sapna Bechoe <s.bechoe@rocmondriaan.nl>
2021-04-26 17:33:11 +0200

    Neutraal thema toegevoegd

--- js/controller/Controller.js
+++ js/controller/Controller.js
@@ -4,28 +4,30 @@ function Controller() //klasse
 {
 	var myView;
 	var myModel;
-	
+
 	this.execute=execute;
 	this.reageerOpKlikNieuwSpel=reageerOpKlikNieuwSpel;
 	this.reageerOpKlikResetSpel=reageerOpKlikResetSpel;
 	this.reageerOpKlikSpeelveld=reageerOpKlikSpeelveld;
-	this.reageerOpKlikRadiobuttonThema1=reageerOpKlikRadiobuttonThema1;
-	this.reageerOpKlikRadiobuttonThema2=reageerOpKlikRadiobuttonThema2;
-	
+	this.reageerOpKlikRadiobuttonNeutraal=reageerOpKlikRadiobuttonNeutraal;
+	this.reageerOpKlikRadiobuttonMeisje=reageerOpKlikRadiobuttonMeisje;
+	this.reageerOpKlikRadiobuttonJongen=reageerOpKlikRadiobuttonJongen;
+
 	function execute()
 	{
 		myView=new ViewBeheerder();
 		myModel=new BKE();
-		
+
 		//Koppelen aan knoppen.
 		document.querySelector('button').onclick=reageerOpKlikNieuwSpel;
 		document.querySelector('#reset').onclick=reageerOpKlikResetSpel;
 		document.getElementById('speelveld').onclick=reageerOpKlikSpeelveld;
 		var radio = document.querySelectorAll('input[type="radio"]');
-		radio[0].onclick=reageerOpKlikRadiobuttonThema1;
-		radio[1].onclick=reageerOpKlikRadiobuttonThema2;
-		radio[1].checked=true;
-		
+		radio[0].onclick=reageerOpKlikRadiobuttonNeutraal;
+		radio[1].onclick=reageerOpKlikRadiobuttonMeisje;
+		radio[2].onclick=reageerOpKlikRadiobuttonJongen;
+		radio[2].checked=true;
+
 		myView.toonMededeling("Welkom, Klik op nieuw spel om te beginnen.");
 	}
 
@@ -43,7 +45,7 @@ function Controller() //klasse
 			myView.toonScoreSpeler2(myModel.geefScoreSpeler2());
 		}
 	}
-	
+
 	function reageerOpKlikResetSpel()
 	{
 		myModel.zetSpelNietActief();
@@ -52,7 +54,7 @@ function Controller() //klasse
 		myModel.resetSpeler2();
 		myView.toonMededeling("Klik op nieuw spel om helemaal opnieuw te beginnen.");
 	}
-	
+
 	function reageerOpKlikSpeelveld(e)
 	{
 		var bron,deGekliktePlek,aanDeBeurt,symbool;
@@ -61,9 +63,9 @@ function Controller() //klasse
 		{
 			bron=e.target;
 			deGekliktePlek=myView.geefGekliktePlek(bron);
-			
+
 			var zetMag=myModel.magZet(deGekliktePlek)
-			//window.alert(zetMag);			
+			//window.alert(zetMag);
 			if(zetMag==true)
 			{
 				var naamSpeler1 = myModel.geefNaamSpeler1();
@@ -76,7 +78,7 @@ function Controller() //klasse
 				var controle = myModel.controleerVeld();
 				myModel.wisselBeurt();
 
-				
+
 				if(aanDeBeurt=="X")
 				{
 					myView.toonMededeling(naamSpeler2+" is aan de beurt.");
@@ -95,9 +97,20 @@ function Controller() //klasse
 			}
 		}
 	}
-	
+
 	//Wisselen van thema's door middel van een radiobutton
-	function reageerOpKlikRadiobuttonThema1()
+	function reageerOpKlikRadiobuttonNeutraal()
+	{
+		document.querySelector('#wrapper').style.background="#ebebeb";
+		document.querySelector('header').style.background="#FFFFFF";
+		document.querySelector('#speelveld').style.background="#000000";
+		document.querySelector('#start').style.color="#000000";
+		document.querySelector('#reset').style.color="#000000";
+		document.querySelector('footer').style.background="#FFFFFF";
+		document.querySelector('span').style.color="#FFFFFF";
+	}
+
+	function reageerOpKlikRadiobuttonMeisje()
 	{
 		document.querySelector('#wrapper').style.background="#F0B1E9";
 		document.querySelector('header').style.background="#F8D1F7";
@@ -105,9 +118,10 @@ function Controller() //klasse
 		document.querySelector('#start').style.color="rgb(255, 0, 245)";
 		document.querySelector('#reset').style.color="rgb(255, 0, 245)";
 		document.querySelector('footer').style.background="#F8D1F7";
+		document.querySelector('span').style.color="#000000";
 	}
-	
-	function reageerOpKlikRadiobuttonThema2()
+
+	function reageerOpKlikRadiobuttonJongen()
 	{
 		document.querySelector('#wrapper').style.background="#6BDAFF";
 		document.querySelector('header').style.background="#B4F9FA";
@@ -115,5 +129,6 @@ function Controller() //klasse
 		document.querySelector('#start').style.color="blue";
 		document.querySelector('#reset').style.color="blue";
 		document.querySelector('footer').style.background="#B4F9FA";
+		document.querySelector('span').style.color="#000000";
 	}
 }
